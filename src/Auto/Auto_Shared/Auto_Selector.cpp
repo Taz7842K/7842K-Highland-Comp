958e07f3bc6a88749c39d80b265e9fa8ae134173
Taz7842K <44256695+Taz7842K@users.noreply.github.com>
2018-12-19 18:24:20 -0800

    Compiles! nothing finished

--- src/Auto/Auto_Shared/Auto_Selector.cpp
+++ src/Auto/Auto_Shared/Auto_Selector.cpp
@@ -1,9 +1,48 @@
 #include "main.h"
+#include "Auto/Shared_Auto/Auto_Selector.h"
+
 
 pros::ADIPotentiometer pot_auto_select('A');
 
+int e_auto_select = pot_auto_select.get_value();
+
+int gui_auto_select = -1;
+
 extern void f_auto_select()
 {
-  if (pot_auto_select < 100)
-  {}
+  if (e_auto_select < 4095)
+  {
+    gui_auto_select = 0;
+    auto_0();
+  }
+
+  else if (e_auto_select < 4095)
+  {
+    gui_auto_select = 1;
+    auto_1();
+  }
+
+  else if (e_auto_select < 4095)
+  {
+    gui_auto_select = 2;
+    auto_2();
+  }
+
+  else if (e_auto_select < 4095)
+  {
+    gui_auto_select = 3;
+    auto_3();
+  }
+
+  else if (e_auto_select < 4095)
+  {
+    gui_auto_select = 4;
+    auto_4();
+  }
+
+  else if (e_auto_select < 4095)
+  {
+    gui_auto_select = 5;
+    auto_5();
+  }
 }
