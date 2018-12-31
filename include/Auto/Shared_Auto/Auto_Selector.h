3e355c213496558e97e9f5f0be5f7a6e21e1bb6d
Taz7842K <44256695+Taz7842K@users.noreply.github.com>
2018-12-31 16:37:17 -0500

    Why do good grills like bad fries

---
+++ include/Auto/Shared_Auto/Auto_Selector.h
@@ -0,0 +1,8 @@
+#include "main.h"
+
+extern void auto_0();
+extern void auto_1();
+extern void auto_2();
+extern void auto_3();
+extern void auto_4();
+extern void auto_5();
---
+++ src/Auto/Auto_Shared/Auto_Selector.cpp
@@ -0,0 +1,74 @@
+#include "main.h"
+#include "Auto/Shared_Auto/Auto_Selector.h"
+
+pros::ADIPotentiometer pot_auto_select('A');
+
+int e_auto_select = pot_auto_select.get_value();
+
+
+ std::string f_gui_auto_select(std::string gui_auto_select)
+{
+  if (e_auto_select < 4095)
+  {
+    return "0_Skills";
+  }
+
+  else if (e_auto_select < 4095)
+  {
+    return "1_Blue_front";
+  }
+
+  else if (e_auto_select < 4095)
+  {
+    return "2_Blue_Rear";
+  }
+
+  else if (e_auto_select < 4095)
+  {
+    return "3_Red_Front";
+  }
+
+  else if (e_auto_select < 4095)
+  {
+    return "4_Red_Rear";
+  }
+
+  else
+  {
+    return 0;
+  }
+}
+
+void f_auto_select(void*)
+{
+ if (e_auto_select < 4095)
+ {
+   auto_0();
+
+ }
+
+ else if (e_auto_select < 4095)
+ {
+   auto_1();
+ }
+
+ else if (e_auto_select < 4095)
+ {
+   auto_2();
+ }
+
+ else if (e_auto_select < 4095)
+ {
+   auto_3();
+ }
+
+ else if (e_auto_select < 4095)
+ {
+   auto_4();
+ }
+
+ else if (e_auto_select < 4095)
+ {
+   auto_5();
+ }
+}
