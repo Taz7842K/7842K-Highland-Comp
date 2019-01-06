dbece1d4ad0e208244bdee942118597c53e9064c
Taz7842K <44256695+Taz7842K@users.noreply.github.com>
2019-01-06 14:03:21 -0800

    ready for testing pid

--- src/Auto/Auto_0_Skills.cpp
+++ src/Auto/Auto_0_Skills.cpp
@@ -1,15 +1,20 @@
 #include "main.h"
+#include "auto/auto_config.h"
 
 void auto_0(void*)
 {
 
   mecBase.setmaxVelocity(200);
-  pros::delay(20);
-
-  mecBase.moveDistance(5_in);
-  pros::delay(20);
-
-  mecBase.turnAngle(90_deg);
+  pros::delay(100);
 
   ballshoot();
+
+  mecBase.turnAngle(90_deg);
+  pros::delay(100);
+
+  mecBase.moveDistance(40_in);
+  pros::delay(100);
+
+  mecBase.moveDistance()
+
 }
